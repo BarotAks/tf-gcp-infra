337e92656c6a7014547b6a35e9c01ed002097ee2
BarotAks <114847017+BarotAks@users.noreply.github.com>
2024-02-15 03:31:15 -0500

    Ft
    * adding tf files
    
    * changing var file

--- main.tf
+++ main.tf
@@ -1,33 +1,37 @@
 # Create a VPC
 resource "google_compute_network" "my_vpc" {
-  name                    = "my-vpc"
+  name                    = var.vpc_name
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
 }
 
 # Create a subnet for webapp
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
+  name          = var.webapp_subnet_name
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
-  ip_cidr_range = "10.0.1.0/24"
+  ip_cidr_range = var.webapp_subnet_cidr
 }
 
 # Create a subnet for db
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
+  name          = var.db_subnet_name
   region        = var.region
   network       = google_compute_network.my_vpc.self_link
-  ip_cidr_range = "10.0.2.0/24"
+  ip_cidr_range = var.db_subnet_cidr
 }
 
-# Add a route for webapp subnet
+# Create an intenet gateway
+resource "google_compute_global_address" "gateway_ipv4" {
+  name         = var.internet_gateway_name
+  purpose      = "GATEWAY"
+  address_type = "EXTERNAL"
+}
+
+# Create a route for webapp subnet
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-route"
+  name             = var.webapp_route_name
   network          = google_compute_network.my_vpc.self_link
   dest_range       = "0.0.0.0/0"
   next_hop_gateway = "default-internet-gateway"
 }
-
-
-
